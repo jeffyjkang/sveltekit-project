cb3674f0fc642e4f9c28f087b24444c1a985c61b
jeff <jeffyjkang@gmail.com>
2021-12-22 21:52:00 -0600

    update

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,52 +1,38 @@
 <script lang='ts'>
+  import { goto } from '$app/navigation';
   import { theme } from '../theme';
   const dark = theme.global.text.dark;
   const light = theme.global.text.light;
   const neutral = theme.global.text.neutral;
+  
+  const navigate = (type: string) => {
+    goto(`/themes/${type}`)
+  }
 </script>
 
 <header>
   <nav>
-    <a
-      href='/'
+    <button
       style='
         --border: {dark};
         --background: {neutral};
         --color: {light};
       '
     >
-      Home
-    </a>
-    <a
-      href='/themes/base'
-      style='
-        --border: {dark};
-        --background: {neutral};
-        --color: {light};
-      '
-    >
-      Base Theme
-    </a>
-    <a
-      href='/themes/neon'
-      style='
-        --border: {dark};
-        --background: {neutral};
-        --color: {light};
-      '
-    >
-      Neon Theme
-    </a>
-    <a
-      href='/themes/pastel'
-      style='
-        --border: {dark};
-        --background: {neutral};
-        --color: {light};
-      '
-    >
-      Pastel Theme
-    </a>
+    <a href='/' style='text-decoration: none; color: inherit;'>HOME</a>
+    </button>
+    {#each ['base', 'neon', 'pastel'] as type}
+      <button
+        on:click|preventDefault = {() => navigate(type)}
+        style='
+          --border: {dark};
+          --background: {neutral};
+          --color: {light};
+        '
+      >
+        {type.toUpperCase()} COLORS
+      </button>
+    {/each}
   </nav>
 </header>
 
@@ -60,14 +46,14 @@
     display: flex;
     flex-flow: row wrap;
   }
-  a {
-    text-decoration: none;
+  button {
     border-radius: 8px;
     padding: 4px;
     margin: 4px;
     border: 2px solid var(--border);
     background: var(--background);
     color: var(--color);
+    cursor: pointer;
   }
 </style>
 
---
+++ src/routes/themes/[type].svelte
@@ -0,0 +1,32 @@
+<script lang='ts'>
+  import { theme } from '../../theme';
+  import { page } from '$app/stores';
+</script>
+
+<div class='theme-container'>
+  {#each ['dark', 'light', 'neutral'] as mode}
+    <h1
+      style='
+        --color: {theme[mode][`primary`][$page.params.type]};
+        --background: {theme[mode][`secondary`][$page.params.type]};
+      '
+    >
+      {mode} theme
+    </h1>
+  {/each}
+</div>
+
+<style>
+  .theme-container {
+    display: flex;
+    flex-flow: row wrap;
+  }
+  h1 {
+    margin: 8px;
+    padding: 8px;
+    border-radius: 8px;
+    color: var(--color);
+    background: var(--background);
+  }
+</style>
+
